40c85f2eca2912929faebaef6a5a49ee39a9f60e
Snaol <99713315+Snaol@users.noreply.github.com>
2025-01-31 17:15:57 -0600

    added Signup

--- php/mainpage.php
+++ php/mainpage.php
@@ -8,8 +8,22 @@
 </head>
 
 <body>
-    <div>
+    <div class="grid-container">
+        <nav class="navbar">
+            <div class="navbar__logo">
+                <a href="mainpage.php">GameScout</a>
+            </div>
+            <div class="navbar__menu">
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href="signup.php">Signup</a></li>
+                </ul>
+            </div>
 
+        </nav>
     </div>
 </body>
 

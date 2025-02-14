39508672f9236a23242d93b8094975b8c0107369
Krutik <ksr912@uregina.ca>
2025-02-14 13:14:34 -0600

    Created Wishlist page
    Added the skeleton code for a Users wishlist, added an image placeholder for user's profile and implemented skeleton of how the wishlist should look and function

---
+++ php/wishlist.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GameScout</title>
+    <link rel="stylesheet" href="../css/style.css" />
+</head>
+
+<body>
+    <nav class="nav_bar">
+        <div class="nav_div">
+            <div class="logo">
+                <a href="#"> <img src="../images/GameScout-logo.png" alt=""> </a>
+            </div>
+            <input type="text" placeholder="Search">
+            <ul>
+                <li><a href="signup.php"><img src="../images/person.png" alt=""></a></li>
+                <li><a href=""><img src="../images/heart.png" alt=""></a></li>
+            </ul>
+        </div>
+    </nav>
+    <div>
+        <img src="../images/BlankAvatar.png" alt="Avatar" width="200"> 
+        <span style="font-size: 30px;color:black;">Undefined's Wishlist</span>
+    </div>
+    <div>
+        <span style="font-size: 16px;color:black">Filter </span>
+        <select size="1">
+            <option>Date Added</option>
+            <option>Price</option>
+        </select>
+        <input type="text" placeholder="Search Wishlist">
+    </div>
+
+</body>

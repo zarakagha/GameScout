e5d0f8e283b99945444fe117f227e8c3d2390254
Krutik <ksr912@uregina.ca>
2025-02-19 14:31:46 -0600

    List of all games acquired from using steam
    There seems to be a bug where there are duplicate games in the list, not sure why will have to look more into it

--- backend/steam.py
+++ backend/steam.py
@@ -1,7 +1,32 @@
 import requests
+import json
+import re
 
 #https://api.steampowered.com/{interface}/{method}/{version}/?key={your_api_key}&{parameters}
 
-steamResponse = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=SETKEYHERE")
+
+#files that will contain game names
+fileout = open("Gameslist.txt", mode="w")
+fileoutappid = open("Gameslistwithappid.txt", mode="w")
+
+#Getting the list of games from steam
+steamResponse = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=STEAMKEYHERE")
 steamGamesList = steamResponse.json()
-print(steamGamesList)
+gameString = json.dumps(steamGamesList)
+
+#acquire the names and appid's of the game in question
+list_of_games = re.findall(r'("name".+?})', gameString)
+list_of_games_with_appid = re.findall(r'({"appid".*?})', gameString)
+
+#for games with the name only, remove the bracket and print toi its respective file
+for game in list_of_games:
+    game = game.replace("}", "")
+    print(game, file=fileout)
+
+#Games with app ids
+for gameappid in list_of_games_with_appid:
+    print(gameappid, file=fileoutappid)
+
+
+fileoutappid.close()
+fileout.close()

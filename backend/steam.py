3f3eff17d4b80590274d42596cf3f7cf508506e6
Krutik <ksr912@uregina.ca>
2025-02-21 12:54:50 -0600

    Fixed duplicate games bug, using dictionary and retreiving games correctly and fully
    All games are now being received correctly

--- backend/steam.py
+++ backend/steam.py
@@ -11,29 +11,38 @@ fileoutappidandname = open("Gameslistwithappidandname.txt", mode="w")
 fileoutraw = open("RawGameList.json", mode="w")
 fileoutappid = open("gamelistappid.txt", mode="w")
 
+#initalize dictionary for no duplicates
+gamedict = {}
+
 #Getting the list of games from steam
 steamResponse = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=STEAMKEYHERE")
 steamGamesList = steamResponse.json()
 gameString = json.dumps(steamGamesList)
 json.dump(steamGamesList, fileoutraw)
 
-#acquire the names and appid's of the game in question
-list_of_games = re.findall(r'("name".+?})', gameString)
+
 list_of_games_with_appid_and_name = re.findall(r'({"appid".*?})', gameString)
-list_of_games_appid = re.findall(r'("appid".*?,)', gameString)
 
-#for games with the name only, remove the bracket and print toi its respective file
-for game in list_of_games:
-    game = game.replace("}", "")
-    print(game, file=fileout)
-
-#Games with app ids
+#Games with unique app ids and names
 for gameappidandname in list_of_games_with_appid_and_name:
-    print(gameappidandname, file=fileoutappidandname)
-    
-for gameappid in list_of_games_appid:
-    gameappid = gameappid.replace(",", "")
-    print(gameappid, file=fileoutappid)
+    #match game app id, and game name in string
+    currgameappidmatch = re.search(r'("appid".*?,)', gameappidandname)
+    currgamenamematch = re.search(r'("name".+?})', gameappidandname)
+    #retreive values for game name and app id
+    currgameappid = currgameappidmatch.group()
+    currgamename = currgamenamematch.group()
+    #clean up game name string
+    currgamename = currgamename.replace("}", "")
+    #add to dictionary so no duplicated values are added
+    if currgameappid not in gamedict:
+        gamedict[currgameappid] = currgamename
+        #print out to files
+        #Appid + gamename file
+        print(gameappidandname, file=fileoutappidandname)
+        #app id only file
+        print(currgameappid, file=fileoutappid)
+        #game name only file
+        print(currgamename, file=fileout)
 
 
 fileoutappidandname.close()

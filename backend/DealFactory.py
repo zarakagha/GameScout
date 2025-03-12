3221c33e328d0674bf52395b4273dcea5dacec00
Krutik <ksr912@uregina.ca>
2025-03-12 16:49:41 -0600

    Factory? complete, getting the game deals and storing them within the game object

--- backend/DealFactory.py
+++ backend/DealFactory.py
@@ -2,11 +2,22 @@ import requests
 import json
 import re
 from gameclass import Game
+from GetSteamPriceCadForGame import get_inital_price
+from ConvertPrice import ConvertUSDToCad
 
 class DealForGameSimpleFactory:
-    
-    def __init__(self, cheapsharkID):
-        self.current_game = Game(cheapsharkID)
         
-    def GetGameDealsAcrossStores(appid):
-        cheapshark
+    def GetGameDealsAcrossStores(cheapsharkgameID):
+        gameObj = Game(cheapsharkgameID)
+        cheapsharkdealsRequest = requests.get("https://www.cheapshark.com/api/1.0/games?id={}".format(gameObj.cheapSharkGameId()))
+        cheapsharkJSON = cheapsharkdealsRequest.json()
+        gameObj.setGameName(cheapsharkJSON["info"]["title"])
+        gameObj.setGameSteamappid(cheapsharkJSON["info"]["steamAppID"])
+        gameObj.setGamePriceCAD(get_inital_price(gameObj.gameSteamAppId()))
+        for deal in cheapsharkJSON["deals"]:
+            gameObj.addToListOfPrices(deal["price"])
+            gameObj.addToListOfStores(deal["storeID"])
+            gameObj.addStoreAndPrice(deal["storeID"], deal["price"])
+            gameObj.addStoreAndSavings(deal["storeID"], deal["savings"])
+        ConvertUSDToCad.convertListOfPricesFromGame(gameObj)
+        return gameObj

b905f443982960a61d7db3bce9141c6642811ddd
zarakagha <zarak.agha@hotmail.com>
2025-03-21 13:39:23 -0600

    comments to event register login

--- static/js/eventRegisterLogin.js
+++ static/js/eventRegisterLogin.js
@@ -1,2 +1,2 @@
 var form1 = document.getElementById("login-form");
-form1.addEventListener("submit", validateLogin);
+form1.addEventListener("submit", validateLogin); //event listener that calls the login validator

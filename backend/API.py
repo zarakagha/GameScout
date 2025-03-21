30036cf9a9f59a1caf3a0934731438278d5d854e
TracePrice135 <82984598+TP-13579@users.noreply.github.com>
2025-03-21 16:50:13 -0600

    More comments

--- backend/API.py
+++ backend/API.py
@@ -20,151 +20,258 @@ import threading
 import concurrent.futures
 from backend.sessionVariables import SessionData
 
-
+#loads access to users and wishlist tables in the database
 users = database.UsersDatabase()
 WishList = database.WishListDatabase()
 
-
+'''
+This is the main API class which is used to load the games in the main page.
+'''
 class API:
+    #In the constructor the class API is passed the memory address to the session variables and 
+    #stores then as a variable inside the class
     def __init__(self,sessionData):
         self.sessionData = sessionData
 
+    '''
+    This is the main function which calls the API for the main page. The function
+    firs checks if the API's have already been loaded. The function works by 
+    multithreading and calling functions which call multiple APIs at the same time.
+    The function creates four thread for the four gamestore APIs that is must call.
+    It then starts the threads which call the function. It joins the threads then 
+    ends the function.
 
+    Inputs: None
+    Outputs: None
+    Uses: session data,self.cheapsharkAPI,self.epicAPI,self.GOGAPI,self.fanaticalAPI
+    '''
     def runMainAPI(self):
+         # checks if the API has been loaded already
          if not self.sessionData.Loaded:
-              cheepsharkThread = threading.Thread(target = self.cheepsharkAPI)
+              #Uses python threading to create four threads. It sets the target to a function
+              #loading the API
+              cheapsharkThread = threading.Thread(target = self.cheapsharkAPI)
               epicThread = threading.Thread(target = self.epicAPI)
               GOGThread = threading.Thread(target = self.GOGAPI)
               fanaticalThread = threading.Thread(target = self.fanaticalAPI)
 
-              cheepsharkThread.start()
+              #This will start the tread
+              cheapsharkThread.start()
               epicThread.start()
               GOGThread.start()
               fanaticalThread.start()
-
-              cheepsharkThread.join()
+              
+              #This will join all of the threads which means waiting for 
+              #all the treads to end before the parent can proceed
+              cheapsharkThread.join()
               epicThread.join()
               GOGThread.join()
               fanaticalThread.join()
-
+              
+              #sets Loaded to True to indicate that the API's are loaded
               self.sessionData.Loaded = True
 
-    
-    def cheepsharkAPI(self):
+    '''
+    Loads the games from Cheapshark. It sends a request to cheapshark.com for games from
+    that store to be displayed on the main page. The functions stores that values to these calls
+    in a sessiondata dict called steamgamesDict.
+    Input: None
+    Output: None
+    Uses: session vars,api calls
+    '''
+    def cheapsharkAPI(self):
+        #request the api
         steamgames=requests.get("https://www.cheapshark.com/api/1.0/deals?storeID=1")
+        #converts it into a json
         steamgamesjson=steamgames.json()
+        #picks the first 9 games
         steamgamesjson=steamgamesjson[:9]
+        #loops through the games
         for currentdeal in steamgamesjson:
             try:
+                  #gets the price of the game,the discounted price, savings and puts it
+                  #into the sessiondata dictionary
                   OriginalPriceOfGame = get_inital_price(currentdeal["steamAppID"])
                   discounted_price = round(ConvertUSDToCad.convertPriceForGame(float(OriginalPriceOfGame), int(currentdeal["storeID"]), float(currentdeal["savings"])), 2)
                   savings = int(round(float(currentdeal["savings"]), 0))
                   self.sessionData.steamgamesDict[str(currentdeal["steamAppID"])] = [str(OriginalPriceOfGame), discounted_price, savings, currentdeal["title"], currentdeal["gameID"]]
             except:
+                  #error
                   print("Error in creating dictionary for game for Steam, Game name: " + currentdeal["title"] + " Steam App ID value: " + str(currentdeal["steamAppID"]))
                   continue
-            
+    '''
+    Loads the games from epic. It sends a request to cheapshark for epic games for games from
+    that store to be displayed on the main page. The functions stores that values to these calls
+    in a sessiondata dict called steamgamesDict.
+    Input: None
+    Output: None
+    Uses: session vars,api calls
+    '''    
     def epicAPI(self):
+        #request the api
         epicgames=requests.get("https://www.cheapshark.com/api/1.0/deals?storeID=25")
+        #converts it into a json
         epicgamesjson=epicgames.json()
+        #picks the first 9 games
         epicgamesjson=epicgamesjson[:9]
+        #loops through the games
         for currentdeal in epicgamesjson:
             try:
+                  #gets the price of the game,the discounted price, savings and puts it
+                  #into the sessiondata dictionary
                   OriginalPriceOfGame = get_inital_price(currentdeal["steamAppID"])
                   discounted_price = round(ConvertUSDToCad.convertPriceForGame(float(OriginalPriceOfGame), int(currentdeal["storeID"]), float(currentdeal["savings"])), 2)
                   savings = int(round(float(currentdeal["savings"]), 0))
                   self.sessionData.epicgamesDict[str(currentdeal["steamAppID"])] = [str(OriginalPriceOfGame), discounted_price, savings, currentdeal["title"], currentdeal["gameID"]]
             except:
+                  #error
                   print("Error in creating dictionary for game for Epic, Game name: " + currentdeal["title"] + " Steam App ID value: " + str(currentdeal["steamAppID"]))
                   continue
-            
+    '''
+    Loads the games from GOG. It sends a request to cheapshark.com for games from
+    that store to be displayed on the main page. The functions stores that values to these calls
+    in a sessiondata dict called steamgamesDict.
+    Input: None
+    Output: None
+    Uses: session vars,api calls
+    '''
     def GOGAPI(self):
+        #request the api
         goggames=requests.get("https://www.cheapshark.com/api/1.0/deals?storeID=7")
+        #converts it into a json
         goggamesjson=goggames.json()
+        #picks the first 9 games
         goggamesjson=goggamesjson[:9]
+        #loops through the games
         for currentdeal in goggamesjson:
             try:
+                  #gets the price of the game,the discounted price, savings and puts it
+                  #into the sessiondata dictionary
                   OriginalPriceOfGame = get_inital_price(currentdeal["steamAppID"])
                   discounted_price = round(ConvertUSDToCad.convertPriceForGame(float(OriginalPriceOfGame), int(currentdeal["storeID"]), float(currentdeal["savings"])), 2)
                   savings = int(round(float(currentdeal["savings"]), 0))
                   self.sessionData.goggamesDict[str(currentdeal["steamAppID"])] = [str(OriginalPriceOfGame), discounted_price, savings, currentdeal["title"], currentdeal["gameID"]]
             except:
+                  #error
                   print("Error in creating dictionary for game for GOG, Game name: " + currentdeal["title"] + " Steam App ID value: " + str(currentdeal["steamAppID"]))
                   continue
-            
+    
+    '''
+    Loads the games from fanatical. It sends a request to cheapshark.com for games from
+    that store to be displayed on the main page. The functions stores that values to these calls
+    in a sessiondata dict called steamgamesDict.
+    Input: None
+    Output: None
+    Uses: session vars,api calls
+    '''
     def fanaticalAPI(self):
+        #request the api
         fanaticalgames=requests.get("https://www.cheapshark.com/api/1.0/deals?storeID=15")
+        #converts it into a json
         fanaticalgamesjson=fanaticalgames.json()
+        #picks the first 9 games
         fanaticalgamesjson=fanaticalgamesjson[:9]
+        #loops through the games
         for currentdeal in fanaticalgamesjson:
             try:
+                  #gets the price of the game,the discounted price, savings and puts it
+                  #into the sessiondata dictionary
                   OriginalPriceOfGame = get_inital_price(currentdeal["steamAppID"])
                   discounted_price = round(ConvertUSDToCad.convertPriceForGame(float(OriginalPriceOfGame), int(currentdeal["storeID"]), float(currentdeal["savings"])), 2)
                   savings = int(round(float(currentdeal["savings"]), 0))
                   self.sessionData.fanaticalgamesDict[str(currentdeal["steamAppID"])] = [str(OriginalPriceOfGame), discounted_price, savings, currentdeal["title"], currentdeal["gameID"]]
             except:
+                  #Error
                   print("Error in creating dictionary for game for Fanatical, Game name: " + currentdeal["title"] + " Steam App ID value: " + str(currentdeal["steamAppID"]))
                   continue
             
-            
+    '''
+    RunWishAPI: This function calls the API to load the game details for the wishlist.
+    It also uses multithreading and it does this by breaking the wishlist into groups of 
+    5 and sending each thread to do one of these groups.
+
+    Input: UserID
+    Output: None
+    Uses: sessionData,api calls,database
+    '''
     def runWishAPI(self, userID):
-         
+        #gets the wishlist from the database class using the userid
         sql = "SELECT * FROM WishList WHERE userID = %s;"
         usergameslist = WishList.select(sql,userID)
-        #self.wishAPI(usergameslist)
+        
+        #sets thread size
         size = 5
+        #creates a list with threads
         threads = [0]*((len(usergameslist)-1)//size + 1)
+        #creates a mutex lock for the sessiondata dictionary
         mutex_lock = threading.Lock()
+        #loops through all the threads
         for i in range((len(usergameslist)-1)//size + 1):
-            
+            #checks if it is the last thread
             if size*(i+1) >= len(usergameslist):
+                #makes thread contain remaining items
                 threads[i] = threading.Thread(target = self.wishAPI, args = ( usergameslist[size*i:],mutex_lock))
             else:
-                threads[i] = threading.Thread(target = self.wishAPI, args = (  usergameslist[size*i:size*i+size],mutex_lock))
+                #puts 5 items from the wishlist into a thread and calls wishAPI function
+                threads[i] = threading.Thread(target = self.wishAPI, args = (  usergameslist[size*i:size*i+size],mutex_lock))#
 
+        #starts all the threads
         for i in range(len(threads)):
              threads[i].start()
-
+        #joins all the threads
         for i in range(len(threads)):
              threads[i].join()
         
              
+    '''
+    wishAPI: the main API function that actually calls the API to load the wishlist. it loops through the usergameslist
+    calls the API and updates the session data
 
+    Inputs: wishlist for this thread, mutex lock to protect the wishlist
+    Outputs: None
+    Uses: sessionData,api calls,database
+    '''
     def wishAPI(self,usergameslist,lock):      
-        
+        #loops through the games
         for game in usergameslist:
+            #gets gameid
             id=game["gameID"]
+            #looks accross stores for game price
             CurrGameObj = DealForGameSimpleFactory.GetGameDealsAcrossStores(id)
+            #turns into a proper dictionary 
             wishlist=CurrGameObj.getWishListFormatDict()
             AcquiredWishlistList = wishlist[str(CurrGameObj.gameSteamAppId())]
+            #finds initial price, lowest price, savings, title
             inital_price = str(AcquiredWishlistList[0])
             lowest_price = AcquiredWishlistList[1]
             savings = AcquiredWishlistList[2]
             title = AcquiredWishlistList[3]
-            #gamedetails=requests.get("https://www.cheapshark.com/api/1.0/games?id={}".format(id))
-            #gamedetailsjson=gamedetails.json()
-                         
-                                                            
-            #OriginalPriceOfGame = get_inital_price(gamedetailsjson["info"]["steamAppID"])
-            #discounted_price,store = ConvertUSDToCad.getDiscountedPrice(gamedetailsjson["deals"])
-            #savings = int(round((1.0 - discounted_price/float(OriginalPriceOfGame))*100 ))
+            #locks sessionData.gamedetailDict which is a shared resource
             lock.acquire()
+            #updates sessionData
             self.sessionData.gamedetailDict[CurrGameObj.gameSteamAppId()] = [inital_price, lowest_price, savings, title, id, 0]
+            #releases lock
             lock.release()
 
+    '''
+    addToWishList: used to add a single element to a wishlist
+
+    Inputs: id for the user
+    Outputs: None
+    Uses: sessionData,api calls,database
+    '''
     def addToWishList(self,id):
+        #finds prices of game accross stores
         CurrGameObj = DealForGameSimpleFactory.GetGameDealsAcrossStores(id)
+        #formats the object
         wishlist=CurrGameObj.getWishListFormatDict()
         AcquiredWishlistList = wishlist[str(CurrGameObj.gameSteamAppId())]
+        #finds initial price, lowest price, savings, title
         inital_price = str(AcquiredWishlistList[0])
         lowest_price = AcquiredWishlistList[1]
         savings = AcquiredWishlistList[2]
         title = AcquiredWishlistList[3]
-        #gamedetails=requests.get("https://www.cheapshark.com/api/1.0/games?id={}".format(id))
-        #gamedetailsjson=gamedetails.json()
-                                                            
-        #discounted_price,store = ConvertUSDToCad.getDiscountedPrice(gamedetailsjson["deals"])
-        #savings = int(round((1.0 - discounted_price/float(OriginalPriceOfGame))*100 ))
+        #puts data in session variable
         self.sessionData.gamedetailDict[CurrGameObj.gameSteamAppId()] = [inital_price, lowest_price, savings, title, id,0]
         
     
--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,24 @@
 import pymysql
 
+
+'''
+Base class for the database classes. It is used to connect to the database and open it.
+'''
 class Database:
+    #initalizer picks up the name of the database
     def __init__(self):
           self.dbname = "GameScout"
 
+    '''
+    Function to open the database
+    
+    Input: None
+    Output: database connection variable
+    Uses: Database
+    '''
     def DBOpen(self):
         timeout = 10
+        #call to open database
         connection = pymysql.connect(
          charset="utf8mb4",
          connect_timeout=timeout,
@@ -22,29 +35,53 @@ class Database:
     
 
 
-
+'''
+Class for the user database
+'''
 class UsersDatabase(Database):
     def __init__(self):
         super().__init__()
     
+    '''
+    Inserts an elements into the users database. 
 
+    Input: firstname string, lastname string, username string, email string, password string isAdmin bool
+    Output:None
+    Uses: database
+    '''
     def insert(self,firstname,lastname, username, email,password,isAdmin = False ):
+        #opens the database using parent class
         connection = self.DBOpen()
-
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
             sql = "INSERT INTO Users (firstname,lastname,username,email,password,isadmin) VALUES (%s, %s,%s,%s,%s,%s)"
+            #exicutes Sql
             cursor.execute(sql,(firstname,lastname,username,email,password,isAdmin))
         finally:
+            #commits changes and closes
             connection.commit()
             connection.close()
-    def select(self,SQL,*args):
-        connection = self.DBOpen()
+    '''
+    Pushes sql into the database
 
+    Input: SQL command,arguments given for the s1l
+    Output:values
+    Uses: database
+    '''  
+    def select(self,SQL,*args):
+        #opens the database using parent class
+        connection = self.DBOpen()
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
+            #exicutes Code
             cursor.execute(SQL,args)
             value = cursor.fetchall()
         finally:
@@ -52,7 +89,13 @@ class UsersDatabase(Database):
             connection.close()
 
         return value
-    
+    '''
+    Pushes sql into the database
+
+    Input: SQL command,arguments given for the s1l
+    Output:values
+    Uses: database
+    '''
     def remove(self,SQL,*args):
         connection = self.DBOpen()
 
@@ -67,78 +110,130 @@ class UsersDatabase(Database):
 
         return value
 
+'''
+Class for wishlist database
+'''
 
 class WishListDatabase(Database):
     def __init__(self):
         super().__init__()
 
     def insert(self,userId,gameId, price):
+        #opens the database using parent class
         connection = self.DBOpen()
-
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
             sql = "INSERT INTO WishList (userID,gameID,price) VALUES (%s, %s,%s)"
             cursor.execute(sql,(userId,gameId,price))
         finally:
             connection.commit()
             connection.close()
-      
-    def select(self,SQL,*args):
-        connection = self.DBOpen()
+    '''
+    Pushes sql into the database
 
+    Input: SQL command,arguments given for the s1l
+    Output:values
+    Uses: database
+    '''
+    def select(self,SQL,*args):
+        #opens the database using parent class
+        connection = self.DBOpen()
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
+            #exicutes sql and gets values
             cursor.execute(SQL,args)
             value = cursor.fetchall()
         finally:
+            #commits connection and closes
             connection.commit()
             connection.close()
 
         return value
     
-    def remove(self,SQL,*args):
-        connection = self.DBOpen()
+    '''
+    Pushes sql into the database
 
+    Input: SQL command,arguments given for the s1l
+    Output:values
+    Uses: database
+    '''
+    def remove(self,SQL,*args):
+        #opens the database using parent class
+        connection = self.DBOpen()
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
+            #exicute sql
             cursor.execute(SQL,args)
             value = cursor.fetchall()
         finally:
+            #commits and closes database
             connection.commit()
             connection.close()
 
         return value
 
-
+'''
+Class for gamestore database
+'''
 class Gamestores(Database):
     def __init__(self):
         super().__init__()
 
    
-      
-    def select(self,SQL,*args):
-        connection = self.DBOpen()
+    '''
+    Pushes sql into the database
 
+    Input: SQL command,arguments given for the s1l
+    Output:values
+    Uses: database
+    '''
+    def select(self,SQL,*args):
+        #opens the database using parent class
+        connection = self.DBOpen()
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
+            #exicutes code
             cursor.execute(SQL,args)
             value = cursor.fetchall()
         finally:
+            #commits and closes database
             connection.commit()
             connection.close()
 
         return value
-    
-    def update(self,SQL,*args):
-        connection = self.DBOpen()
+    '''
+    Pushes sql into the database
 
+    Input: SQL command,arguments given for the s1l
+    Output:values
+    Uses: database
+    '''
+    def update(self,SQL,*args):
+        #opens the database using parent class
+        connection = self.DBOpen()
+        #try to handle errors
         try:
+            #connects a cursor to send calls
             cursor = connection.cursor()
+            #uses database
             cursor.execute("USE GameScout;")
+            #exi
             cursor.execute(SQL,args)
             value = cursor.fetchall()
         finally:
